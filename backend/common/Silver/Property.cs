472805a45bc11c2961bc2e677b9474fc9dc50b06
hearthfire12 <solomoychenkoo@gmail.com>
2023-01-15 19:54:21 +0200

    Update Property.cs

--- backend/common/Silver/Property.cs
+++ backend/common/Silver/Property.cs
@@ -5,8 +5,3 @@ public class Property
     public string Name { get; init; }
     public List<object> Values { get; init; } = new();
 }
-
-public class TransformProperty : Property
-{
-    public string? Alias { get; init; }
-}
