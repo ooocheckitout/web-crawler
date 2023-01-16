65ced1a7e39a5656e64b9cca9872bccf5a5b3f80
hearthfire12 <solomoychenkoo@gmail.com>
2023-01-16 13:10:17 +0200

    add schema and urls controllers

---
+++ backend/analytics.api/Controllers/SchemasController.cs
@@ -0,0 +1,54 @@
+﻿using common;
+using common.Bronze;
+using common.Silver;
+using Microsoft.AspNetCore.Mvc;
+
+namespace analytics.api.Controllers;
+
+[ApiController]
+[Route("collections/{collection}/schemas")]
+public class SchemasController : ControllerBase
+{
+    readonly CollectionLocator _locator;
+    readonly FileReader _fileReader;
+    readonly FileWriter _fileWriter;
+
+    public SchemasController(CollectionLocator locator, FileReader fileReader, FileWriter fileWriter)
+    {
+        _locator = locator;
+        _fileReader = fileReader;
+        _fileWriter = fileWriter;
+    }
+
+    [HttpGet]
+    [Route("bronze")]
+    public Task<ParserSchema> GetParserSchema(string collection, CancellationToken cancellationToken)
+    {
+        string schemaLocation = _locator.GetSchemaLocation(collection, Medallion.Bronze);
+        return _fileReader.ReadJsonAsync<ParserSchema>(schemaLocation, cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("bronze")]
+    public Task CreateParserSchema(string collection, ParserSchema schema, CancellationToken cancellationToken)
+    {
+        string schemaLocation = _locator.GetSchemaLocation(collection, Medallion.Bronze);
+        return _fileWriter.AsJsonAsync(schemaLocation, schema, cancellationToken);
+    }
+
+    [HttpGet]
+    [Route("silver")]
+    public Task<TransformerSchema> GetTransformSchema(string collection, CancellationToken cancellationToken)
+    {
+        string schemaLocation = _locator.GetSchemaLocation(collection, Medallion.Silver);
+        return _fileReader.ReadJsonAsync<TransformerSchema>(schemaLocation, cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("silver")]
+    public Task CreateTransformSchema(string collection, ParserSchema schema, CancellationToken cancellationToken)
+    {
+        string schemaLocation = _locator.GetSchemaLocation(collection, Medallion.Silver);
+        return _fileWriter.AsJsonAsync(schemaLocation, schema, cancellationToken);
+    }
+}
---
+++ backend/analytics.api/Controllers/UrlsController.cs
@@ -0,0 +1,36 @@
+﻿using common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace analytics.api.Controllers;
+
+[ApiController]
+[Route("collections/{collection}/urls")]
+public class UrlsController : ControllerBase
+{
+    readonly CollectionLocator _locator;
+    readonly FileReader _fileReader;
+    readonly FileWriter _fileWriter;
+
+    public UrlsController(CollectionLocator locator, FileReader fileReader, FileWriter fileWriter)
+    {
+        _locator = locator;
+        _fileReader = fileReader;
+        _fileWriter = fileWriter;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public Task<IEnumerable<string>> GetUrls(string collection, CancellationToken cancellationToken)
+    {
+        string urlsLocation = _locator.GetUrlsLocation(collection);
+        return _fileReader.ReadJsonAsync<IEnumerable<string>>(urlsLocation, cancellationToken);
+    }
+
+    [HttpPost]
+    [Route("")]
+    public Task CreateUrls(string collection, IEnumerable<string> urls, CancellationToken cancellationToken)
+    {
+        string urlsLocation = _locator.GetUrlsLocation(collection);
+        return _fileWriter.AsJsonAsync(urlsLocation, urls, cancellationToken);
+    }
+}
